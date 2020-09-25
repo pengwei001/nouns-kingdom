cc8dc9ba645ca656b676c9ad25b14af8945398ff
nouns-kingdom <andrew.xue.gong@outlook.com>
2020-09-25 15:06:49 -0400

    add ga tracking code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import gtag from "vue-gtag-next";
+// import gtag from "vue-gtag-next";
 
 const app = createApp(App);
-app.use(gtag, {
-  property: {
-    id: "UA-179016977-1"
-  }
-});
+// app.use(gtag, {
+//   property: {
+//     id: "UA-179016977-1"
+//   }
+// });
 app.use(router).mount("#app");
