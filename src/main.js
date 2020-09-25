bfbe9bc4377e9ffaab7205fe19fc286d4f072fc8
nouns-kingdom <andrew.xue.gong@outlook.com>
2020-09-24 21:07:29 -0400

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueAnalytics from "vue-analytics";
 
 createApp(App)
-  .use(router)
+  .use(VueAnalytics, {
+    id: "UA-179016977-1",
+    router
+  })
   .mount("#app");
